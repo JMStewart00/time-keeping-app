eeb9ecdef2a6ba6ddd004aaf1a6d2b8af17ed687
Robert Bryan <bryarobert@gmail.com>
2017-06-29 16:34:45 -0400

    resolved merge conflicts for header add tasks

--- app/php/task_header_bar.php
+++ app/php/task_header_bar.php
@@ -1,36 +1,12 @@
 
 <nav class="text-center navbar fixed-top d-block bg-inverse">
 	<form action="GET">
-<<<<<<< HEAD
-=======
 		<input type="hidden" name="nextClientId" value="<?=$nextTaskId ?>">
->>>>>>> selectClient
 		<div class="container p-0">
 			<div class="row" id="topNav">
 				<div class="input-group col-4 inputTopNav">
 					<span class="input-group-addon">Task</span>
 					<input type="text" class="form-control" aria-label="Text input" name="task_name" id="task_name" placeholder="Enter task name here...">
-<<<<<<< HEAD
-				</div>
-				<div class="button-group col inputTopNav" id="selectClient">
-					<button class="btn btn-inline-block dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Select Client</button>
-					<div class="dropdown-menu" aria-labelledby="dropdownMenu1">
-						<a class="dropdown-item" href="#">Action</a>
-						<a class="dropdown-item" href="#">Action</a>
-						<a class="dropdown-item" href="#">Action</a>
-					</div>
-				</div>
-				<div class="input-group col inputTopNav">
-					<label type="text" class="input-group-addon" aria-label="Text input with checkbox">Rate</label>
-					<label type="text" class="input-group-addon" aria-label="Text input with checkbox">$</label>
-					<label class="input-group-addon" for="fader" style="width: 50px;"><output for="fader" id="volume">100</output></label>
-					<input class="ml-2" type="range" min="5" max="200" value="150" id="fader" step="5" name="rate" oninput="outputUpdate(value)">
-				</div>
-				<button class="btn  btn-outline-success" value="add_task" name="submit" type="submit"><i class="fa fa-play" aria-hidden="true"></i></button>
-			</div>
-		</nav>
-	</form>
-=======
 				</div>
 				<div class="col select-style">
 					<select name="client_id">
@@ -52,4 +28,4 @@
 			</div>
 		</form>
 	</nav>
->>>>>>> selectClient
+
